77a2e51e302e7db3e39dae0aa529d30053033b63
alep-2000 <alessiopiras056@gmail.com>
2023-07-07 15:00:22 +0200

    Create 2 istance

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+// CLASS MOVIE
+class Movie {
+    // public $title;
+    // public $yearOfRelease;
+    // public $type;
+    // public $duration;
+    // public $cast;
+    // public $language;
+
+}
+
+$senzaLimiti = new Movie();
+$arteDellaTruffa = new Movie();
+
+
 
 ?>
 
