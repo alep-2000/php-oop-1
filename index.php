27255ff7bc9d19e741cc0de16909c0551b58711e
alep-2000 <alessiopiras056@gmail.com>
2023-07-10 09:58:17 +0200

    Add correction

--- index.php
+++ index.php
@@ -34,24 +34,13 @@ $senzaLimiti = new Movie("Senza Limiti", "2000", "Drammatic", "100 minutes");
 $arteDellaTruffa = new Movie("L'arte della truffa", "2019", "Detective Story", "100 minutes");
 $xxx = new Movie("XXX: Il ritorno di Xander Cage", "2017", "Action", "107 minutes");
 
-$senzaLimiti->title = "Senza Limiti";
-$senzaLimiti->yearOfRelease = "2000";
-$senzaLimiti->type = "Drammatic";
-$senzaLimiti->duration = "100 minutes";
+
 $senzaLimiti->cast = ["Sofiane Zermani", "Camille Rowe", "César Domboy", "Zacharie Chasseriaud", "Laurent Fernandez", "Jonas Dinal", "Natalie Mitson", "Belen Enguidanos", "Hassam Ghancy", "Alexandre Poole", "Antonin Schopfer", "Grégory Gaule"];
 $senzaLimiti->language = "FR";
 
-$arteDellaTruffa->title = "L'arte della truffa";
-$arteDellaTruffa->yearOfRelease = "2019";
-$arteDellaTruffa->type = "Detective Story";
-$arteDellaTruffa->duration = "100 minutes";
 $arteDellaTruffa->cast = ["Theo James", "Emily Ratajkowski", "Aris Alvarado", "Fernanda Andrade", "Tim Bader", "Joe Bucaro III", "Frank Clem", "Taji Coleman", "Davie-Blue", "Frank Gallegos", "John Gatins", "Evan Handler", "Orion McCabe", "Fred Melamed", "Ebon Moss-Bachrach", "Ivo Nandi", "Dominic Pace", "Keith Powell", "Bob Stephenson", "Ray Stoney", "Phil Van Tee"];
 $arteDellaTruffa->language = "US";
 
-$xxx->title = "XXX: Il ritorno di Xander Cage";
-$xxx->yearOfRelease = "2017";
-$xxx->type = "Action";
-$xxx->duration = "107 minutes";
 $xxx->cast = ["Vin Diesel", "Donnie Yen", "Deepika Padukone", "Kris Wu", "Ruby Rose", "Tony Jaa"];
 $xxx->language = "US";
 
